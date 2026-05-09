cad873b985aae1ae5dde3de2bf087aea7c6c933b
112345brian <11creepman@gmail.com>
2026-05-09 13:07:08 -0700

    Point updates to rating-on-left

--- rate-your-music-modern.js
+++ rate-your-music-modern.js
@@ -1,13 +1,13 @@
 // ==UserScript==
 // @name         Rate Your Music Modern
 // @namespace    github.com/112345brian/rate-your-music-modern
-// @version      0.3.4
+// @version      0.3.5
 // @description  Behavior enhancements for the Rate Your Music Modern userstyle.
 // @author       bri
 // @homepageURL  https://github.com/112345brian/rate-your-music-modern
 // @supportURL   https://github.com/112345brian/rate-your-music-modern/issues
-// @updateURL    https://raw.githubusercontent.com/112345brian/rate-your-music-modern/main/rate-your-music-modern.js
-// @downloadURL  https://raw.githubusercontent.com/112345brian/rate-your-music-modern/main/rate-your-music-modern.js
+// @updateURL    https://raw.githubusercontent.com/112345brian/rate-your-music-modern/rating-on-left/rate-your-music-modern.js
+// @downloadURL  https://raw.githubusercontent.com/112345brian/rate-your-music-modern/rating-on-left/rate-your-music-modern.js
 // @match        https://rateyourmusic.com/*
 // @match        http://127.0.0.1:5173/*
 // @match        http://localhost:5173/*
