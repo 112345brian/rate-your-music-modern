fb71926b1fe8674a8eb0ffddb4bc1afb4c821c4f
112345brian <11creepman@gmail.com>
2026-05-16 11:45:02 -0700

    1.5.3

--- rate-your-music-modern.js
+++ rate-your-music-modern.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Rate Your Music Modern
 // @namespace    github.com/112345brian/rate-your-music-modern
-// @version      1.5.2
+// @version      1.5.3
 // @description  Behavior enhancements for the Rate Your Music Modern userstyle.
 // @author       bri
 // @homepageURL  https://github.com/112345brian/rate-your-music-modern
